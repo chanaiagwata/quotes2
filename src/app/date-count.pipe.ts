6787fb75946eabc2c84af3cce616530253c9e8e8
chanaiagwata <chanaiagwata82@gmil.com>
2022-10-23 14:20:08 +0300

    fixed date counter bug

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,12 +8,12 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number {
     let today:Date = new Date(); //get current date
     let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-    let dateDifference = Math.abs(value-todayWithNoTime); //returns value in miliseconds
-    let dateDifferenceInSec = dateDifference * 0.001 //converts dateDifference to seconds
+    let dateDifference = Math.abs(todayWithNoTime-value); //returns value in miliseconds
     let secondsInDay = 86400; //60 sec *60 min *24 hrs in a day
+    let dateDifferenceInSec = dateDifference * 0.001 //converts dateDifference to seconds
     var dateCounter = dateDifferenceInSec / secondsInDay;
 
-    if (dateCounter >= 1 && value> todayWithNoTime){
+    if (dateCounter >= 1 && todayWithNoTime > value){
       return dateCounter
     }else{
       return 0;
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,9 +10,9 @@ import { Quotes } from '../quotes';
 export class QuoteComponent implements OnInit {
   quotes: Quotes[] = [
 
-    new Quotes(1, 'Technology is best when it brings people together','Matt Mullenweg', new Date(2022,10,18)),
-    new Quotes(2, 'It has become appallingly obvious that our technology has exceeded our humanity', 'Albert Einstein', new Date(2022, 10, 18)),
-    new Quotes(3, 'If it keeps up, man will atrophy all his limbs but the push-button finger', 'Frank Lloyd Wright', new Date(2022, 10, 18))
+    new Quotes(1, 'Technology is best when it brings people together','Matt Mullenweg', new Date(2022, 9, 22)),
+    new Quotes(2, 'It has become appallingly obvious that our technology has exceeded our humanity', 'Albert Einstein', new Date(2022, 9, 18)),
+    new Quotes(3, 'If it keeps up, man will atrophy all his limbs but the push-button finger', 'Frank Lloyd Wright', new Date(2022, 9, 18))
   ];
   toggleDetails(index:number){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
